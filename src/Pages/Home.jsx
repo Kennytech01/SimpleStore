abd698e272067e97e2d37a558fe3318a8bd10bff
Kennytech01 <Kennysonvictory@gmail.com>
2023-11-08 11:57:50 +0100

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -33,17 +33,17 @@ import {TbFaceIdError} from 'react-icons/tb'
         {
             (searchItem == '') &&
                 <div>
-                    <div id='hero' className='group relative  max-h-screen bg-violet-300/20'>
-                        <div className='min-h-full flex md:justify-start justify-end items-center relative p-5 blur-[1px md:blur-non'>
-                            <img src={Shopping} alt="" className=' object-cover object-center md:rounded-full p-1 lg:w-[32rem] w-[22rem] lg:h-[32rem] h-[22rem] border-[#781d75]/30 md:border-2 md:-scale-x-100 scale-x-100 '/>
+                    <div id='hero' className='group relative  max-h-screen bg-violet-300/20 flex items-center md:justify-betwee justify-around '>
+                        <div className='min-h-full flex md:justify-start justify-end items-center relative p-5 blur-[1px md:blur-non '>
+                            <img src={Shopping} alt="" className=' object-cover object-center lg:rounded-full lg:w-[32rem] w-[22rem] lg:h-[32rem] h-[22rem] border-[#781d75]/30 lg:border-2 -scale-x-100 '/>
                         </div>
-                        <div className="items-center justify-center h-full ">
-                            <div className='bg-stone-2 rounded flex flex-col justify-center h-full mx-4 absolute top-0 lg:left-1/2 md:left-1/4 sm:left-10 left-2 duration-500 transition-all z-10'>
-                                <p className=' text-[#752d42] text-4xl flex items-center'>
-                                    <span id='welcome'>-welcome there!</span> 
-                                    <BsEmojiSunglasses className='text-stone-700 ml-1'/>
+                        <div className="items-center justify-center h-80 sm:relative absolute left-0 right-0 mx-5 duration-500 transition-all">
+                            <div className='bg-stone-2 rounded flex flex-col justify-center p-2 relative drop-shadow border-[#781d75] bg-violet-500/10 sm:bg-black/0 h-full'>
+                                <p className=' text-[#752d42] text-4xl flex flex-wrap items-center'>
+                                    <span id='welcome' className='text-stone-20 sm:text-[#781d75 font-bold bg-gradient-to-r bg-clip-text text-transparent to-[#781d75] from-[#EC094D] to[#fb923c] flex items-center'>Welcome <span className='text-orange-400 pl-2'>there!</span></span> 
+                                    {/* <BsEmojiSunglasses className='text-stone-700 ml-1'/> */}
                                 </p>
-                                <h2 className='p-2 py-5 font-light bg-gradient-to-r bg-clip-text text-transparent from-[#781d75] to-[#EC094D] text-normal md:text-xl'>Get your order delivered at your doorsteps all day everyday</h2>
+                                <h2 className='p-2 py-5 bg-gradient-to-r bg-clip-text sm:text-transparent from-[#781d75] to-[#EC094D] text-xl text-white '>Get your order delivered at your doorsteps all day everyday</h2>
                                 <button className='py-4 font-bold text-stone-100 text-xl flex'>
                                     <a href='#products' className='bg-[#781d75] rounded p-2 hover:opacity-80 cursor-pointer transition-all flex items-center'>
                                         Shop now
@@ -56,7 +56,7 @@ import {TbFaceIdError} from 'react-icons/tb'
                     <div className='text-center flex justify-center items-center h-20'>
                         <p className='flex capitalize items-center sm:text-3xl text-xl bg-gradient-to-r bg-clip-text text-transparent from-[#781d75] to-[#EC094D] '>
                             Shop Big Save BIGGER!...
-                            <FaShopify size={40} className='ml-2 text-[#781d75]'/>
+                            <FaShopify size={40} className='ml-2 text-[#781d75] animate-bounce'/>
                         </p>
                     </div>
                 </div>
