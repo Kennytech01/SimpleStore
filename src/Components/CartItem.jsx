9e5bd7390ddeac56c5c28a5aef70bccb817b6f90
Kennytech01 <Kennysonvictory@gmail.com>
2023-11-16 16:08:43 +0100

    update

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -11,24 +11,24 @@ export const CartItem = ({item}) => {
 
   return ( 
     <div className='flex group'>
-        <div className='w-full min-h-28 border flex items-center gap-x-4 m-3 p-2 rounded-lg'>
-            <div>
+        <div className='w-full min-h-28 border flex items-center  my-2 md:mx-10 sm:mx-5 p-2 rounded-lg'>
+            <div className='p-2'>
                 <img src={images[0]} alt={item.title} className='max-w-[80px]' />
             </div>
             <div className=' p-2 w-full m- flex flex-col justify-between'>
                 <h1 className='text-stone-700'>{title}</h1>
-                <p className='flex justify-between items-center my- w-full'>
+                <p className='flex justify-between items-center py-2 w-full'>
                     <span className=' text-[#fb923c] group-hover:cursor-pointer'>{`$${parseFloat(price * amount).toLocaleString()}`}</span>
-                    <span onClick={()=> removeCart(id)} className='text-[#fb923c] cursor-pointer  sm:border border-[#fb923c] flex items-center p-1 rounded'>
+                    <span onClick={()=> removeCart(id)} className='text-[#fb923c] hover:opacity-70 cursor-pointer sm:border border-[#fb923c] flex items-center p-1 rounded'>
                         <BsTrash3/> 
                         <span className='sm:flex hidden'>REMOVE</span>
                     </span>
                 </p>
-                <p className='border px-3 py-1 rounded-lg flex items-center justify-between text-sm m-2 w-32 '>
+                <span className='border px-3 py-1 rounded-lg flex items-center justify-between text-sm m-2 sm:w-32 w-28 '>
                     <BsDashLg onClick={()=> decreaseAmount(id)} className='cursor-pointer'/>
                     <span className='px-5 font-bold'>{amount}</span>
                     <BsPlusLg onClick={()=> increaseAmount(id)} className='cursor-pointer'/>
-                </p>
+                </span>
             </div>
         </div>
     </div>
